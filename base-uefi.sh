314a408d51eeae8df1c2a95c3bad5fa150ff5d78
Ark-Sec <64931479+Ark-Sec@users.noreply.github.com>
2022-11-23 11:12:01 +0200

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -10,7 +10,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S efibootmgr networkmanager  network-manager-applet dialog wpa_supplicant mtools dosfstools  base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb  nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils  pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion  openssh rsync acpi acpi_call tlp virt-manager qemu  qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat  iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid  os-prober terminus-font`
+pacman -S efibootmgr networkmanager  network-manager-applet dialog wpa_supplicant mtools dosfstools  base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb  nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils  pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion  openssh rsync acpi acpi_call tlp virt-manager qemu  qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat  iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid  os-prober terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
